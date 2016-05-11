5d1e52e0f5dd6d6d5d24a958688c66b48e069ea2
jicjjang <jicjjang12@gmail.com>
2016-05-11 23:13:56 +0900

    add test file.

---
+++ Test.py
@@ -0,0 +1,11 @@
+from ML import SVM
+from ML import GetBinaryImage
+
+input_image = 'img/2.png/'
+reference_image = 'ref.png'
+learnData = 'data/learnData/'
+
+binary_image = GetBinaryImage.BImage(input_image)
+binary_image.save_binary_to_image('save')
+
+print SVM.BSVM.get_bald_SVM(input_image, reference_image, learnData)
