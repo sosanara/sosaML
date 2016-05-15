52aa56866f2d42f11a37b1bb8d3478307020cdfb
jicjjang <jicjjang12@gmail.com>
2016-05-16 03:53:24 +0900

    change wrong wording

--- ML/GetBinaryImage.py
+++ ML/GetBinaryImage.py
@@ -25,20 +25,20 @@ class BImage:
         elif temp.find('.bmp') != -1: return temp.split('.bmp')[0]
         elif temp.find('.BMP') != -1: return temp.split('.BMP')[0]
 
-    def _image_background(self, img_name, brg_choice, brg_compare, brg_value):
+    def _image_background(self, img_name, bgr_choice, bgr_compare, bgr_value):
         change_convert_image = Image.open(img_name).convert('RGB')
         img = cv2.imread(self.input_image)
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
-                if img[i][j][brg_choice] > brg_compare:
-                    change_convert_image.putpixel((j, i), brg_value)
+                if img[i][j][bgr_choice] > bgr_compare:
+                    change_convert_image.putpixel((j, i), bgr_value)
         change_convert_image.save(img_name, "PNG")
 
-    def save_binary_to_image(self, path, brg_choice, brg_compare, brg_value):
+    def save_binary_to_image(self, path, bgr_choice, bgr_compare, bgr_value):
         path = Preprocessing.append_slash(path)
         img_name = path + 'binary_' + self.image_name + '.png'
         cv2.imwrite(img_name, self.binary_image)
-        self._image_background(img_name, brg_choice, brg_compare, brg_value)
+        self._image_background(img_name, bgr_choice, bgr_compare, bgr_value)
 
         return {
             'filename': 'binary_' + self.image_name,
