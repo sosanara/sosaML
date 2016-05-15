0110f0f96557998fbc726c99949b11ce1e7df523
jicjjang <jicjjang12@gmail.com>
2016-05-16 03:12:13 +0900

    add background preprocessor. (color change)

--- ML/GetBinaryImage.py
+++ ML/GetBinaryImage.py
@@ -1,15 +1,18 @@
+from PIL import Image
 from Preprocessing import Preprocessing
 import cv2
 
 
 class BImage:
+    input_image = None
     binary_image = None
     image_name = None
     path = None
 
     def __init__(self, input_image):
+        self.input_image = Preprocessing.remove_slash(input_image)
         self.binary_image = Preprocessing.detect_skin_color(input_image)
-        self.image_name = self._image_name(Preprocessing.remove_slash(input_image))
+        self.image_name = self._image_name(self.input_image)
 
     def _image_name(self, input_image):
         temp = input_image.split('/')[-1]
@@ -22,10 +25,21 @@ class BImage:
         elif temp.find('.bmp') != -1: return temp.split('.bmp')[0]
         elif temp.find('.BMP') != -1: return temp.split('.BMP')[0]
 
-    def save_binary_to_image(self, path):
+    def _image_background(self, img_name, brg_choice, brg_compare, brg_value):
+        change_convert_image = Image.open(img_name).convert('RGB')
+        img = cv2.imread(self.input_image)
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                if img[i][j][brg_choice] > brg_compare:
+                    change_convert_image.putpixel((j, i), brg_value)
+        change_convert_image.save(img_name, "PNG")
+
+    def save_binary_to_image(self, path, brg_choice, brg_compare, brg_value):
         path = Preprocessing.append_slash(path)
         img_name = path + 'binary_' + self.image_name + '.png'
         cv2.imwrite(img_name, self.binary_image)
+        self._image_background(img_name, brg_choice, brg_compare, brg_value)
+
         return {
             'filename': 'binary_' + self.image_name,
             'path': path,
